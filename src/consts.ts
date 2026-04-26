9f5198880db00b31fabbe09d741173819d869b82
Pranav Jadhav <pra168109@gmail.com>
2026-04-25 21:24:32 -0700

    Personalize homepage content

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Miniblog";
-export const SITE_DESCRIPTION =
-  "An opinionated and minimal Astro blog template.";
-export const SITE_URL = "https://miniblog.nicholasly.com/";
+export const SITE_TITLE = "Pranav Jadhav";
+export const SITE_DESCRIPTION = "Personal website and blog for Pranav Jadhav.";
+export const SITE_URL = "http://localhost:4321/";
